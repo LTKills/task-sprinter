48722878028bc41e62017e6582920ad8cdd995bb
diproray <diprorayy@gmail.com>
2018-10-06 17:40:06 -0500

    added high level documentation for routine and task functions

--- src/routine.py
+++ src/routine.py
@@ -1,21 +1,45 @@
 
 
 class Routine:
+    '''
+    Each Routine is a list of tasks, but with more information.
+    Currently, the extra information is Description and Total Time.
 
+    A routine ends when the last task in the routine is completed.
+    '''
+
+    '''
+    Set the title for the routine.
+    '''
     def __init__(self, title):
         self.title = title
 
+    '''
+    Return the title of the routine.
+    '''
     def __str__(self):
         return self.title
 
+    '''
+    Start the execution of the list of tasks within the routine.
+    '''
     def start(self):
         pass
 
+    '''
+    Add a new task to the list of tasks within the routine.
+    '''
     def add(self):
         pass
 
+    '''
+    Remove a task from the list of tasks within the routine.
+    '''
     def remove(self):
         pass
 
+    '''
+    Edit the description for the routine.
+    '''
     def edit(self):
         pass
